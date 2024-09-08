b2845c2de76bdd005c524a9b9c657d9a722704b5
Aline870 <aline.ns008@gmail.com>
2024-09-08 17:51:33 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,21 @@
+let dados = [
+    {
+        titulo :"Answer: Love Myself",
+        descricao : "A música Answer: Love Myself do BTS nos leva a uma profunda reflexão sobre o amor-próprio. A letra explora a luta interna que muitos enfrentam para se aceitar e superar a insegurança. A metáfora do inverno dando lugar à primavera simboliza a jornada de transformação pessoal, onde a escuridão da autocrítica é substituída pela luz da autoaceitação. O BTS nos encoraja a perdoar nossos erros e a acreditar em nosso próprio valor, transmitindo uma mensagem poderosa de esperança e positividade. A canção é um convite para que cada um de nós encontre a força interior necessária para amar a si mesmo e construir uma vida mais feliz e plena.",
+        link : "https://youtu.be/oBX8xDJjgg4?si=LhMcrRJFcT251ZD6",
+        tags : "interna", 
+    },
+    { 
+        titulo: "Epiphany",
+        descricao : "Epiphany é uma canção do BTS que nos convida a uma profunda jornada de autodescoberta. A música, interpretada por Jin, fala sobre a importância de se amar acima de tudo. No início, o eu lírico se vê perdido em tentar agradar aos outros, colocando as necessidades deles acima das suas. Porém, ao longo da canção, ele realiza uma importante descoberta: o amor mais verdadeiro é o amor-próprio. A letra de Epiphany é como um espelho que reflete a luta de muitas pessoas para se aceitarem como são. A música nos mostra que é normal se sentir perdido e inseguro, mas que é possível encontrar a felicidade ao reconhecer e amar todas as partes de nós mesmos. Através de sua melodia suave e letra inspiradora, Epiphany se torna um hino para aqueles que buscam se conectar consigo mesmos e encontrar a paz interior",
+        link : "https://youtu.be/G9QtQKgxKSM?si=XGIpCpaMVxEk53Iy",
+        tags : "eu", 
+    } ,
+    { 
+        titulo: "Magic Shop",
+        descricao : "Magic Shop é uma canção do BTS que nos transporta para um lugar especial, um refúgio interior onde podemos encontrar cura e conforto. Essa loja mágica é um espaço metafórico onde os fãs do BTS podem compartilhar suas emoções, medos e inseguranças, encontrando apoio e força para superar as dificuldades.  A música fala sobre a importância de acreditar em si mesmo e de aceitar quem somos, com todas as nossas qualidades e imperfeições. O BTS nos convida a explorar nosso mundo interior, a galáxia que cada um carrega dentro de si, e a reconhecer o nosso valor único. A canção também aborda a pressão que sentimos para sermos perfeitos e como isso pode nos afetar. Em vez disso, o BTS nos incentiva a sermos nós mesmos e a sermos uma fonte de apoio para os outros. Magic Shop é mais do que apenas uma música, é um convite para uma jornada de autoconhecimento e autoaceitação. É uma mensagem de esperança e positividade que nos lembra que não estamos sozinhos e que todos temos o poder de superar nossos desafios. A canção fortalece o vínculo entre o BTS e seus fãs, criando um sentimento de comunidade e união.",
+        link : "https://youtu.be/znmKqBO5uNc?si=Na29kxRrrgtFcvya",
+        tags : "shop",
+    }
+   
+];
